151fb473ed80152ddb52047ed087e34d5017b478
Ben Norris <ben@bsn.io>
2016-03-08 10:18:46 -0700

    Add blank framework

---
+++ LaunchCount/LaunchCount.h
@@ -0,0 +1,19 @@
+//
+//  LaunchCount.h
+//  LaunchCount
+//
+//  Created by Ben Norris on 3/8/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaunchCount.
+FOUNDATION_EXPORT double LaunchCountVersionNumber;
+
+//! Project version string for LaunchCount.
+FOUNDATION_EXPORT const unsigned char LaunchCountVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaunchCount/PublicHeader.h>
+
+
